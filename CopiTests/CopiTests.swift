b72b5425e6b601dcaeaa0d11798863fba269e280
gillianarce <103377170+gillianarce@users.noreply.github.com>
2026-05-06 01:53:00 -0600

    Add files via upload

---
+++ CopiTests/CopiTests.swift
@@ -0,0 +1,17 @@
+//
+//  CopiTests.swift
+//  CopiTests
+//
+//  Created by Kxze on 05/05/26.
+//
+
+import Testing
+@testable import Copi
+
+struct CopiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
